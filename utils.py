8fa40d82ccd7dea6cf4788229ef61565119de430
ppomega <prateekveerbhan405@gmail.com>
2025-11-23 11:41:32 +0530

    update

--- utils.py
+++ utils.py
@@ -10,7 +10,7 @@ from random import choices # requires Python >= 3.6
 import numpy as np
 import cv2
 import torch
-from skimage.measure.simple_metrics import compare_psnr
+from skimage.metrics import peak_signal_noise_ratio as compare_psnr
 from tensorboardX import SummaryWriter
 
 IMAGETYPES = ('*.bmp', '*.png', '*.jpg', '*.jpeg', '*.tif') # Supported image types
